e935a6e81017b00cf108a841d403cc8a1151022b
unknown <sonhoangdang0612@gmail.com>
2025-05-08 06:05:20 +0700

    test case 01: same txnRef

--- src/modules/payments/payments.service.ts
+++ src/modules/payments/payments.service.ts
@@ -92,14 +92,14 @@ export class PaymentsService extends TypeOrmBaseService<Payment> {
       let vnp_Params: Record<string, any> = {
         vnp_Version: version,
         vnp_Command: 'pay',
-        //vnp_TmnCode: tmnCode,
+        vnp_TmnCode: tmnCode,
         vnp_Locale: 'vn',
         vnp_CurrCode: 'VND',
-        vnp_TxnRef: txnRef,
+        vnp_TxnRef: '1',
         vnp_OrderInfo: orderInfo,
         vnp_OrderType: 'other',
         vnp_Amount: totalPrice * 100,
-        //vnp_ReturnUrl: returnUrl,
+        vnp_ReturnUrl: returnUrl,
         vnp_IpAddr: ipAddress,
         vnp_CreateDate: dayjs().format('YYYYMMDDHHmmss'),
       };
